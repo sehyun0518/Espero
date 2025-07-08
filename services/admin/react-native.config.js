0df636417016a94480a7bac5b97e997f4de61ab2
SeHyun <rla6170@naver.com>
2025-07-08 13:31:31 +0900

    chore: link asset files(design-system) -> admin service

---
+++ services/admin/react-native.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  assets: [
+    path.resolve(__dirname, '../../packages/design-system/src/assets/fonts'),
+    path.resolve(__dirname, '../../packages/design-system/src/assets/icons'),
+    path.resolve(__dirname, '../../packages/design-system/src/assets/images'),
+  ],
+};
