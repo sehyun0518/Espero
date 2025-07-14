719c528673353a5d7a3f1363a8292739172ff34a
SeHyun <rla6170@naver.com>
2025-07-14 16:49:30 +0900

    docs: add icon atoms component

---
+++ packages/design-system/src/lib/stories/Icon/index.stories.tsx
@@ -0,0 +1,18 @@
+import { Icon } from '../../../components/atoms/Icon';
+
+const meta = {
+  title: 'ESD/Icon',
+  component: Icon,
+  argTypes: {},
+};
+
+export default meta;
+
+export const Primary = {
+  args: {
+    name: 'alert',
+    color: 'black',
+    width: 24,
+    height: 25,
+  },
+};
