00a51c595c166cda4c9b7699111023201a69e885
SeHyun <rla6170@naver.com>
2025-07-15 14:52:30 +0900

    docs: add divider component storybook

---
+++ packages/design-system/src/lib/stories/Divider/index.stories.tsx
@@ -0,0 +1,20 @@
+import { Divider } from '../../../components/atoms/Divider';
+
+const meta = {
+  title: 'ESD/Divider',
+  component: Divider,
+  argTypes: {},
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    direction: 'horizon',
+    weight: 'thin',
+    style: {
+      width: 300,
+      height: 300,
+    },
+  },
+};
